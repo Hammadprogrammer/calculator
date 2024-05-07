ba0f38b2bd5c3cb1b776bdc638ae396d3e2833f6
Hammadprogrammer <154420287+Hammadprogrammer@users.noreply.github.com>
2024-05-07 09:48:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById('display').value = displayValue;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
